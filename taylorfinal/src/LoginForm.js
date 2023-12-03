07dd50b7478b9f87f14b963270faf90b2ad90bf1
Jane Swingler <130300157+therealslimjaney@users.noreply.github.com>
2023-12-03 15:27:50 -0800

    Update LoginForm.js
    with comments

--- taylorfinal/src/LoginForm.js
+++ taylorfinal/src/LoginForm.js
@@ -1,66 +1,78 @@
+/**
+ * LoginForm component handles user authentication using Firebase.
+ * @param {function} setGoogleId - Callback function from parent component to set Google ID.
+ * @returns {JSX.Element} - Rendered login form with Google sign-in button.
+ */
 import { initializeApp } from 'firebase/app';
 import { getAuth, signInWithRedirect, onAuthStateChanged, GoogleAuthProvider } from 'firebase/auth';
 import React, { useState, useEffect } from 'react';
 import './App.css';
 
-// LoginSuccessful is a function sent in by parent component
-function LoginForm({ setGoogleId}) {
-	const firebaseConfig = {
-		apiKey: "AIzaSyCPuHAVpGtTiz4LwApdWVSJIFJPF2dieLQ",
-		authDomain: "taylor-e20bf.firebaseapp.com",
-		projectId: "taylor-e20bf",
-		storageBucket: "taylor-e20bf.appspot.com",
-		messagingSenderId: "211296375786",
-		appId: "1:211296375786:web:e35b697c58eb3bee995893",
-		measurementId: "G-S5H931JN91"
-	};
+function LoginForm({ setGoogleId }) {
+  // Firebase configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyCPuHAVpGtTiz4LwApdWVSJIFJPF2dieLQ",
+    authDomain: "taylor-e20bf.firebaseapp.com",
+    projectId: "taylor-e20bf",
+    storageBucket: "taylor-e20bf.appspot.com",
+    messagingSenderId: "211296375786",
+    appId: "1:211296375786:web:e35b697c58eb3bee995893",
+    measurementId: "G-S5H931JN91"
+  };
 
-	initializeApp(firebaseConfig);
-	const [user, setUser] = useState(null);
-	const signInWithGoogle = () => {
-		const provider = new GoogleAuthProvider();
-		const auth = getAuth();
+  // Initialize Firebase app
+  initializeApp(firebaseConfig);
 
-		signInWithRedirect(auth, provider)
-			.then(() => {
-				// Nothing to do here, result is handled by onAuthStateChanged
-			})
-			.catch((error) => {
-				// Handle Errors here.
-				console.error(error);
-			});
+  // State for current user
+  const [user, setUser] = useState(null);
 
-	};
+  // Function to sign in with Google
+  const signInWithGoogle = () => {
+    const provider = new GoogleAuthProvider();
+    const auth = getAuth();
 
-useEffect(() => {
-  const auth = getAuth();
-  const unsubscribe = auth.onAuthStateChanged((user) => {
-    if (user) {
-      setUser(user);
-      setGoogleId(user.uid);
-      console.log(user.uid);
-    } else {
-      console.log("No user is signed in.");
+    signInWithRedirect(auth, provider)
+      .then(() => {
+        // Nothing to do here, result is handled by onAuthStateChanged
+      })
+      .catch((error) => {
+        // Handle Errors here.
+        console.error(error);
+      });
+  };
+
+  // useEffect to handle authentication state changes
+  useEffect(() => {
+    const auth = getAuth();
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
+      if (user) {
+        setUser(user);
+        setGoogleId(user.uid);
+        console.log(user.uid);
+      } else {
+        console.log("No user is signed in.");
+      }
+    });
+
+    // Call the callback manually to handle the initial state
+    const currentUser = auth.currentUser;
+    if (currentUser) {
+      setUser(currentUser);
+      setGoogleId(currentUser.uid);
     }
-  });
 
-  // Call the callback manually to handle the initial state
-  const currentUser = auth.currentUser;
-  if (currentUser) {
-    setUser(currentUser);
-    setGoogleId(currentUser.uid);
-  }
+    // Clean up the listener on component unmount
+    return () => unsubscribe();
+  }, [setGoogleId]);
 
-  return () => unsubscribe(); // Clean up the listener on component unmount
-}, []);
-	
-	  return (
-		<div>
-		  {!user && (
-			<button onClick={signInWithGoogle}>Sign in with Google</button>
-		  )}
-		</div>
-	  );
-	}
+  // Render the login form with Google sign-in button
+  return (
+    <div>
+      {!user && (
+        <button onClick={signInWithGoogle}>Sign in with Google</button>
+      )}
+    </div>
+  );
+}
 
 export default LoginForm;
