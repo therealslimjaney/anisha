070d0f3d2f7e0cc564f05f8c4c402818b085529f
Jane Swingler <130300157+therealslimjaney@users.noreply.github.com>
2023-12-03 15:26:07 -0800

    Update Gamefeedback.js
    with comments

--- taylorfinal/src/Gamefeedback.js
+++ taylorfinal/src/Gamefeedback.js
@@ -1,10 +1,22 @@
+/**
+ * Gamefeedback component renders feedback based on the game state.
+ * @param {number} guesses - The remaining attempts in the game.
+ * @param {number} score - The player's score.
+ * @param {boolean} hasWon - Indicates if the player has won the game.
+ * @param {string} secretCode - The secret code for the game.
+ * @returns {JSX.Element} - Rendered feedback based on game state.
+ */
 export default function Gamefeedback({ guesses, score, hasWon, secretCode }) {
+  // Default class for styling
   let feedbackClass = "Gamefeedback";
+
+  // Add 'won' class and display winning message if the player has won
   if (hasWon) {
     feedbackClass += " won";
-    return <div >Congratulations, you have won!</div>;
+    return <div>Congratulations, you have won!</div>;
   }
 
+  // Add 'lost' class and display losing message with secret code if no remaining attempts
   if (guesses === 0) {
     feedbackClass += " lost";
     return (
@@ -16,8 +28,9 @@ export default function Gamefeedback({ guesses, score, hasWon, secretCode }) {
     );
   }
 
+  // Display remaining attempts and player's score
   return (
-    <div >
+    <div>
       Remaining attempts: {guesses}
       <br />
       Your Score: {score}
